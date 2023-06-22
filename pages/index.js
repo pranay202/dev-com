7222ffcda907e6a0beae6e00c0a02c64450f1ee8
swag-pranay <pranay@swagstay.in>
2023-06-23 00:32:18 +0530

    UPDATE: Home

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ const Home = (props) => {
   return (
     <div className='relative overflow-hidden'>
       <main>
-        <div className='pt-10 bg-light sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden'>
+        <div className='pt-10 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden'>
           <div className='container mx-auto max-w-7xl lg:px-8'>
             <div className='row lg:gap-8'>
               <div className='mx-auto col-md-8 col-lg-6 col-xl-6 px-4 sm:text-center'>
@@ -19,11 +19,11 @@ const Home = (props) => {
                       Visit our products page
                     </span>
                   </a>
-                  <h1 className='mt-4 mb-6 text-4xl tracking-tight font-weight-bold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl'>
+                  <h1 className='mt-4 mb-6 text-4xl tracking-tight font-weight-bold text-dark sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl'>
                     <span className='d-block'>A better way to</span>
-                    <span className='d-block text-primary'>ship your code</span>
+                    <span className='d-block text-primary'>sell your code</span>
                   </h1>
-                  <p className='mb-6 text-lg text-light sm:mt-5 sm:text-xl lg:text-lg xl:text-xl'>
+                  <p className='mb-6 text-lg text-dark sm:mt-5 sm:text-xl lg:text-lg xl:text-xl'>
                     Request a free sell of your code for better customers reach!
                   </p>
                   <div className='mt-6'>
@@ -47,10 +47,10 @@ const Home = (props) => {
                             type='email'
                             name='email'
                             placeholder='Enter your email'
-                            className='form-control px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-dark'
+                            className='form-control px-4 py-3 rounded-md border-4 text-base text-gray-900 placeholder-gray-500 focus:ring-2 focus:ring-offset-2 focus:ring-primary focus:ring-offset-dark'
                           />
                         </div>
-                        <div className='col-sm-3'>
+                        <div className='col-sm-3 mt-3 mt-md-0'>
                           <button
                             type='submit'
                             style={{ width: '100%' }}
@@ -72,14 +72,50 @@ const Home = (props) => {
                   </div>
                 </div>
               </div>
-              <div className='mt-5 col-lg-6'>
-                <div className='mx-auto max-w-md px-4'>
-                  {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
-                  <img
-                    className='w-full'
-                    src='https://tailwindui.com/img/component-images/cloud-illustration-indigo-400.svg'
-                    alt=''
-                  />
+              <div className='container-sm mx-auto px-4'>
+                <div className='py-12 position-relative mt-12 py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-50'>
+                  {/* <div className='d-none d-sm-block'>
+                    <div className='position-absolute inset-y-0 left-50 w-100 bg-gray-50 rounded-start-3xl lg:left-80 lg:right-0 lg:w-100' />
+                    <svg
+                      className='position-absolute me-3 lg:m-0 lg:left-0'
+                      width={404}
+                      height={392}
+                      fill='none'
+                      viewBox='0 0 404 392'
+                    >
+                      <defs>
+                        <pattern
+                          id='837c3e70-6c3a-44e6-8854-cc48c737b659'
+                          x={0}
+                          y={0}
+                          width={20}
+                          height={20}
+                          patternUnits='userSpaceOnUse'
+                        >
+                          <rect
+                            x={0}
+                            y={0}
+                            width={4}
+                            height={4}
+                            className='text-gray-200'
+                            fill='currentColor'
+                          />
+                        </pattern>
+                      </defs>
+                      <rect
+                        width={404}
+                        height={392}
+                        fill='url(#837c3e70-6c3a-44e6-8854-cc48c737b659)'
+                      />
+                    </svg>
+                  </div> */}
+                  <div className='position-relative pl-4 me-40 mx-auto px-0 lg:h-100 lg:pl-12'>
+                    <img
+                      className='w-100 rounded-md shadow-lg border ring-1 ring-dark ring-opacity-5 lg:h-100'
+                      src='https://tailwindui.com/img/component-images/top-nav-with-multi-column-layout-screenshot.jpg'
+                      alt=''
+                    />
+                  </div>
                 </div>
               </div>
             </div>
